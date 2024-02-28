30609cfe874bd07cb622a006d9b0b8b77d312b43
ChyoRi <chyori_@naver.com>
2024-02-28 17:56:22 +0900

    sixth

--- mo/index.js
+++ mo/index.js
@@ -1,6 +1,8 @@
 const selectBoxBtn = document.querySelector('.total-select-box-btn');
+const selectBoxCurrentText = document.querySelector('.select-box-current');
 const selectBoxList = document.querySelector('.total-select-box-list');
-console.log(selectBoxBtn);
+const selectOptionBtn = document.querySelectorAll('.total-select-box-list > li > button');
+
 
 const selectBoxExpend = (e) => {
   const arrow = e.currentTarget.children[1];
@@ -8,4 +10,17 @@ const selectBoxExpend = (e) => {
   selectBoxList.classList.toggle('active');
 }
 
+const selectOption = (e) => {
+  let target = e.currentTarget;
+  let targetIdx = target.getAttribute('data-id');
+  let targetText = target.innerText;
+  let currentText = selectBoxCurrentText.innerText;
+  selectBoxCurrentText.innerText = targetText;
+  target.innerText = currentText;
+  console.log(targetIdx);
+}
+
 selectBoxBtn.addEventListener('click', selectBoxExpend);
+selectOptionBtn.forEach(item => {
+  item.addEventListener('click', selectOption);
+});
